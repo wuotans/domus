0f9e12a306ca992c9d1c06247830bda19f41a597
Wuotans <107582914+wuotans@users.noreply.github.com>
2026-09-24 09:42:22 -0400

    feat: adiciona types/next-auth.d.ts

---
+++ types/next-auth.d.ts
@@ -0,0 +1,19 @@
+import "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user?: {
+      id?: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+      role?: string;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: string;
+  }
+}
